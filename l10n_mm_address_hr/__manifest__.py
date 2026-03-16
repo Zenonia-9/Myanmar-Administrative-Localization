83b75f3a79c8cb5cf4239776c7b32b7857825878
Zenonia <theinhtooaung906@gmail.com>
2026-03-16 11:39:59 +0630

    - minor version changes

--- l10n_mm_address_hr/__manifest__.py
+++ l10n_mm_address_hr/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Myanmar HR Address Localization',
-    'version': '18.0.2.0',
+    'version': '18.0.7.0',
     'category': 'Human Resources/Localization',
     'summary': 'Myanmar address hierarchy fields for employee private addresses with MIMU P-code integration.',
     'description': """
